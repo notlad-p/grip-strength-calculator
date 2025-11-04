a140d22470b4edb0a4a0b73684aa2347cddd2883
Dalton Perkins <contact@daltonp.dev>
2025-11-04 15:32:19 -0500

    add code link

--- src/App.tsx
+++ src/App.tsx
@@ -10,15 +10,14 @@ import "./index.css";
 import data from "./data.json";
 import ReactSpeedometer from "react-d3-speedometer";
 import { useState } from "react";
+import CodeIcon from "./CodeIcon";
 
 function lerp(start: number, end: number, factor: number) {
   return start + (end - start) * factor;
 }
 
 // TODO: ask what text to display after calculating
-//
 // TODO: show all percentiles chart under? (and where they fall)
-// TODO: show percentile they lay in
 
 type SpedometerDataType = {
   value: number;
@@ -27,12 +26,20 @@ type SpedometerDataType = {
   segmentStops: number[];
 };
 
+const Code = () => {
+  return (
+    <div className="size-5 fill-neutral-400">
+      <CodeIcon />
+    </div>
+  );
+};
+
 export default function App() {
   const [spedometerData, setSpedometerData] = useState<SpedometerDataType>({
     value: 0,
     min: 0,
-    max: 100,
-    segmentStops: [0, 33, 66, 100],
+    max: 40,
+    segmentStops: [0, 10, 30, 40],
   });
   const [forceRender, setForceRender] = useState(false);
 
@@ -41,21 +48,17 @@ export default function App() {
   const [gripStrength, setGripStrength] = useState<string | number>("");
 
   const onCalculateClick = () => {
-    // const sexLower: <'male' | 'female>' = sex.toLowerCase()
     let sexData = data[sex];
     if (!age) {
-      console.log("INPUT AGE");
       return;
     }
 
     if (!gripStrength) {
-      console.log("INPUT GRIP STRENGTH");
       return;
     }
 
     // find age less than `maxAge`, unless over 100
     // loop keys that start with "P" into array & set segment stops
-
     let ageData = sexData.find((val) => val.maxAge > age);
 
     // if age is 100+
@@ -86,24 +89,12 @@ export default function App() {
     });
 
     setTimeout(() => setForceRender(false), 150);
-
-    // for (let key in ageData) {
-    //   if (key.startsWith("P")) {
-    //     console.log(`${key}: ${ageData[key]}`);
-    //   }
-    // }
   };
 
   return (
     <MantineProvider theme={theme} defaultColorScheme="dark">
-      <div className="p-4 max-w-fit mx-auto flex items-center flex-col">
-        {/* <div className="p-4 max-w-10/12 mx-auto"> */}
+      <div className="p-4 max-w-fit h-screen mx-auto flex items-center flex-col">
         <ReactSpeedometer
-          // minValue={19.7}
-          // maxValue={39.1}
-          // value={23.6}
-          // 0, -5 from 50pt , +5 from 50pt, `max`
-          // customSegmentStops={[19.7, 28.6 - 5, 28.6 + 5, 39.1]}
           value={spedometerData.value}
           minValue={spedometerData.min}
           maxValue={spedometerData.max}
@@ -113,6 +104,7 @@ export default function App() {
           customSegmentStops={spedometerData.segmentStops}
           segmentColors={["#ef4444", "#fde047", "#84cc16"]}
           segmentValueFormatter={(val) => `${val} kg`}
+          height={200}
           //customSegmentLabels={[
           //  { text: "P30", position: "OUTSIDE", color: "white" },
           //  { text: "P80", position: "OUTSIDE", color: "white" },
@@ -155,20 +147,21 @@ export default function App() {
 
           <Button onClick={onCalculateClick}>Calculate</Button>
         </div>
-      </div>
 
-      {/* <p>{data.male[0].P5}</p> */}
-      {/* <p>Sex</p> */}
-      {/* <input /> */}
-      {/* <p>Age</p> */}
-      {/* <input /> */}
-      {/* <p>Grip Strength (KG)</p> */}
-      {/* <input /> */}
-      {/* {data.male.map((val) => ( */}
-      {/*   <div> */}
-      {/*     <p>{val.maxAge}</p> */}
-      {/*   </div> */}
-      {/* ))} */}
+        <div className="mt-auto pt-16">
+          <Button
+            component="a"
+            variant="subtle"
+            color="#a3a3a3"
+            leftSection={<Code />}
+            href="https://github.com/notlad-p/grip-strength-calculator"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Code
+          </Button>
+        </div>
+      </div>
     </MantineProvider>
   );
 }
